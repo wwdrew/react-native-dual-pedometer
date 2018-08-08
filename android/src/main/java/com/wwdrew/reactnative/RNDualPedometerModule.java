d9c3b397dd93923784f5cfb86b62796639bd1cee
Drew Miller <poppiestar@users.noreply.github.com>
2018-08-08 15:22:37 +0100

    Added stub bridge methods

--- android/src/main/java/com/wwdrew/reactnative/RNDualPedometerModule.java
+++ android/src/main/java/com/wwdrew/reactnative/RNDualPedometerModule.java
@@ -1,22 +1,37 @@
 
 package com.wwdrew.reactnative;
 
+import android.util.Log;
+
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
 public class RNDualPedometerModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final static String TAG = "RNDualPedometer";
+    private final ReactApplicationContext reactContext;
 
-  public RNDualPedometerModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNDualPedometerModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "RNDualPedometer";
-  }
+    @Override
+    public String getName() {
+        return "RNDualPedometer";
+    }
+
+    @ReactMethod
+    public void startPedometerUpdatesFromDate(String date, Promise promise) {
+        Log.d(TAG, "startPedometerUpdatesFromDate");
+        promise.resolve(true);
+    }
+
+    @ReactMethod
+    public void stopPedometerUpdates(Promise promise) {
+        Log.d(TAG, "stopPedometerUpdates");
+        promise.resolve(true);
+    }
 }
